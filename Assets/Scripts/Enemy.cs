3d9f3a5335f719587ec1240daf2b0725dcde9624
Shan Phylim <shanphylim@hotmail.com>
2015-01-24 14:37:08 -0500

    Made Prefabs, Animations

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Enemy : MonoBehaviour {
+	public int hp=15;
+	public int atk=10;
+	public int def=5;
+	
+	public int currentHP=15;
+
+	// Use this for initialization
+	void Start () {
+		int currentHP=15;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}

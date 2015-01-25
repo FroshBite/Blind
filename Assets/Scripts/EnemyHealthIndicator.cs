8a1631104d80444b80bbc9ebf38c4a71b8ba78e3
Kevin Xing <kxing@uwaterloo.ca>
2015-01-25 17:01:28 -0500

    UI text replacement scripts
    Finally!

--- Assets/Scripts/EnemyHealthIndicator.cs
+++ Assets/Scripts/EnemyHealthIndicator.cs
@@ -2,23 +2,22 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class HealthIndicator : MonoBehaviour {
+public class EnemyHealthIndicator : MonoBehaviour {
 
 	public GameObject playerObject;
 	PlayerStats player;
-
+	
 	Text txt;
 
 	// Use this for initialization
 	void Start () {
 		player = playerObject.GetComponent<PlayerStats>();
 		txt = gameObject.GetComponent<Text>();
-
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		txt.text = "TEST";
+		txt.text = "OTHER TEST";
+	
 	}
 }

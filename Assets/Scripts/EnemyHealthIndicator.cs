20f933e0143e37dbc1da0259587da5e3198c39ce
Kevin Xing <kxing@uwaterloo.ca>
2015-01-25 18:00:54 -0500

    fixed enemy health indicator

--- Assets/Scripts/EnemyHealthIndicator.cs
+++ Assets/Scripts/EnemyHealthIndicator.cs
@@ -18,25 +18,25 @@ public class EnemyHealthIndicator : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (PlayerStats.currentHP > 98) {
-			txt.text = "I'm in trouble";
-		} else if (PlayerStats.currentHP > 90) {
+		if (enemy.currentHP > 98) {
+			txt.text = "This mate ain't so strong!";
+		} else if (enemy.currentHP > 90) {
 			txt.text = "2";
-		} else if (PlayerStats.currentHP > 80) {
+		} else if (enemy.currentHP > 80) {
 			txt.text = "1";
-		} else if (PlayerStats.currentHP > 70) {
+		} else if (enemy.currentHP > 70) {
 			txt.text = "3";
-		} else if (PlayerStats.currentHP > 60) {
+		} else if (enemy.currentHP > 60) {
 			txt.text = "4";
-		} else if (PlayerStats.currentHP > 50) {
+		} else if (enemy.currentHP > 50) {
 			txt.text = "5";
-		} else if (PlayerStats.currentHP > 40) {
+		} else if (enemy.currentHP > 40) {
 			txt.text = "6";
-		} else if (PlayerStats.currentHP > 30) {
+		} else if (enemy.currentHP > 30) {
 			txt.text = "7";
-		} else if (PlayerStats.currentHP > 20) {
+		} else if (enemy.currentHP > 20) {
 			txt.text = "8";
-		} else if (PlayerStats.currentHP > 10) {
+		} else if (enemy.currentHP > 10) {
 			txt.text = "9";
 		} else {
 			txt.text = "10";

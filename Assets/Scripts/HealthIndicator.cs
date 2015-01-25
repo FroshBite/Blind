d127a5417b5f8ea83e426e40227ced436284c186
Kevin Xing <kxing@uwaterloo.ca>
2015-01-25 17:57:35 -0500

    health-based status text in combat

--- Assets/Scripts/HealthIndicator.cs
+++ Assets/Scripts/HealthIndicator.cs
@@ -19,8 +19,28 @@ public class HealthIndicator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (PlayerStats.currentHP < 100) {
+		if (PlayerStats.currentHP > 98) {
 				txt.text = "I'm in trouble";
+		} else if (PlayerStats.currentHP > 90) {
+			txt.text = "2";
+		} else if (PlayerStats.currentHP > 80) {
+			txt.text = "1";
+		} else if (PlayerStats.currentHP > 70) {
+			txt.text = "3";
+		} else if (PlayerStats.currentHP > 60) {
+			txt.text = "4";
+		} else if (PlayerStats.currentHP > 50) {
+			txt.text = "5";
+		} else if (PlayerStats.currentHP > 40) {
+			txt.text = "6";
+		} else if (PlayerStats.currentHP > 30) {
+			txt.text = "7";
+		} else if (PlayerStats.currentHP > 20) {
+			txt.text = "8";
+		} else if (PlayerStats.currentHP > 10) {
+			txt.text = "9";
+		} else {
+			txt.text = "10";
 		}
 	}
 }
